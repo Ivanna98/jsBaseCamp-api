78d1b92ad5a454f3272d88873d1210e9999333c7
Ivanna Novak <ivanna1998.novak@gmail.com>
2019-11-21 09:49:58 +0200

    added moongo conect and shema

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 const express = require('express');
+
 const app = express();
 const moment = require('moment');
+const mongoose = require('mongoose');
+const config = require('./config');
 
 const PORT = process.env.PORT || 3000;
+
+mongoose.connect(config.db.url, { useNewUrlParser: true })
+  .catch((error) => console.log(error.massage));
+mongoose.connection.on('error', (err) => {
+  console.log(err);
+});
 
 const logger = (req, res, next) => {
   console.log(`${req.method}:${req.path} ${moment().format()}`);
   next();
-}
+};
 app.use(logger);
 
 app.use(express.json());
@@ -15,6 +24,6 @@ app.use(express.urlencoded({ extended: false }));
 
 app.get('/ready', (req, res) => {
   res.send('I`m alive');
-})
+});
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
